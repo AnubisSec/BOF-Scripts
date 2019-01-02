fd9ab6f244c1e0308718ba02f8d2cda2df65cc65
TorturedProphet <38790814+TorturedProphet@users.noreply.github.com>
2019-01-02 10:08:30 -0500

    Minor Comments
    Minor Comments

--- fuzzOffset.py
+++ fuzzOffset.py
@@ -2,7 +2,7 @@
 import socket
 import sys
 import time
-from pwn import *
+from pwn import * #pwntools
 
 
 
@@ -15,35 +15,6 @@ host = sys.argv[1]
 port = sys.argv[2]
 data = sys.argv[3]
 
-p = remote(host, port)
-p.sendline(data)
+p = remote(host, port) # connect to host
+p.sendline(data) #send payload (here being the unique string generated from pattern_offset.rb)
 print("Sent evil buffer >:) Check your debugger for the EIP")
-
-
-
-
-'''
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, int(port)))
-s.recv(1024)
-s.send(data.encode('utf-8'))
-s.close()
-print("Evil Buffer sent >:)")
-'''
-
-
-
-
-'''
-while (length < 10000): #continue to send data until bytes == 10000
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((host, int(port)))
-	s.recv(1024)
-	s.send(b"A" * length)
-	s.recv(1024)
-	s.close()
-	time.sleep(2)
-	print("Buffer sent: " + str(length))
-	length += 100
-
-'''
