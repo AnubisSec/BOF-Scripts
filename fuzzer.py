e378b5af73d1c457267baebb7ca9c7361a9addea
TorturedProphet <38790814+TorturedProphet@users.noreply.github.com>
2019-01-02 10:07:04 -0500

    Minor comments
    Minor comments

--- fuzzer.py
+++ fuzzer.py
@@ -16,7 +16,7 @@ length = 100 #inital length of bytes to send to binary
 while (length < 10000): #continue to send data until bytes == 10000
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((host, int(port))) #Connect to host
-	s.recv(1024) #Grab banners(if any)
+	s.recv(1024) #Grab banners (if any)
 	s.send(b"A" * length) #Send payload
 	s.recv(1024)
 	s.close()
